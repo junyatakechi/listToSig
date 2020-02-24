13fac06526481aa498bc1df03fa9de3017e8cb40
junyatakechi <takechi48j@gmail.com>
2020-02-24 23:28:14 +0900

    creating buffer reference.

--- sources/ext.listToSig.cpp
+++ sources/ext.listToSig.cpp
@@ -9,7 +9,7 @@
 using namespace c74::min;
 
 
-class ext_listToSig : public object<ext_listToSig> {
+class ext_listToSig : public object<ext_listToSig>, public vector_operator<> {
 public:
     MIN_DESCRIPTION    {"Opus decoder from list payload."};
     MIN_TAGS        {"utilities"};
@@ -17,24 +17,92 @@ public:
     MIN_RELATED        {"print, jit.print, dict.print"};
 	
     inlet<> input {this, "(list) recieved opus payload.", "list"};
+    inlet<>		index_inlet		{ this, "(signal) Sample index" };
     outlet<> output {this, "(signal) output the decoded sound.", "signal"};
 	
+		buffer_reference buffer { this,
+        MIN_FUNCTION {
+            length.touch();
+            return {};
+        }
+    };
+	
+		attribute<number> length {this, "length", 1000.0, title {"Length (ms)"}, description {"Length of the buffer~ in milliseconds."},
+        setter { MIN_FUNCTION {
+            number new_length = args[0];
+					
+            if (new_length <= 0.0)
+                new_length = 1.0;
+
+            buffer_lock<false> b {buffer};
+            b.resize(new_length);
+
+            return {new_length};
+        }},
+        getter { MIN_GETTER_FUNCTION {
+            buffer_lock<false> b {buffer};
+            return {b.length_in_seconds() * 1000.0};
+        }}
+    };
+
+
+    argument<symbol> name_arg {this, "buffer-name", "Initial buffer~ from which to read.",
+        MIN_ARGUMENT_FUNCTION {
+            buffer.set(arg);
+        }
+    };
+
+    argument<int> channel_arg {this, "channel", "Initial channel to read from the buffer~.",
+        MIN_ARGUMENT_FUNCTION {
+            channel = arg;
+        }
+    };
+
+
+    attribute<int> channel {this, "channel", 1, description {"Channel to read from the buffer~."},
+        setter { MIN_FUNCTION {
+            int n = args[0];
+            if (n < 1)
+                n = 1;
+            return {n};
+        }}
+    };
+
+    attribute<bool> record {this, "record", false, description {"Record into the loop"}};
+
+
+    message<> number_message {this, "number", "Toggle the recording attribute.",
+        MIN_FUNCTION {
+            record = args[0];
+            return {};
+        }
+    };
+
+
+    message<> dspsetup {this, "dspsetup",
+        MIN_FUNCTION {
+           m_one_over_samplerate = 1.0 / samplerate();
+           return {};
+       }
+    };
+
+
 	
     // define an optional argument for setting the message
-    argument<symbol> greeting_arg { this, "greeting", "Initial value for the greeting attribute.",
-        MIN_ARGUMENT_FUNCTION {
-            greeting = arg;
-        }
-    };
-	
-	
-    // the actual attribute for the message
-    attribute<symbol> greeting { this, "greeting", "hello world",
-        description {
-            "Greeting to be posted. "
-            "The greeting will be posted to the Max console when a bang is received."
-        }
-    };
+//    argument<symbol> greeting_arg { this, "greeting", "Initial value for the greeting attribute.",
+//        MIN_ARGUMENT_FUNCTION {
+//            greeting = arg;
+//        }
+//    };
+//
+//
+//    // the actual attribute for the message
+//    attribute<symbol> greeting { this, "greeting", "hello world",
+//        description {
+//            "Greeting to be posted. "
+//            "The greeting will be posted to the Max console when a bang is received."
+//        }
+//    };
 	
 	
     //constructors
@@ -53,49 +121,37 @@ public:
 				}
     };
 	
-    // respond to the bang message to do something
-    message<> bang { this, "bang", "Post the greeting.",
-        MIN_FUNCTION {
-            symbol the_greeting = greeting;
-            cout << the_greeting << endl;
-            output.send(the_greeting);
-            return {};
-        }
-    };
+//    // respond to the bang message to do something
+//    message<> bang { this, "bang", "Post the greeting.",
+//        MIN_FUNCTION {
+//            symbol the_greeting = greeting;
+//            cout << the_greeting << endl;
+//            output.send(the_greeting);
+//            return {};
+//        }
+//    };
     
-    message<> number { this, "float",
-        MIN_FUNCTION {
-            int buf_msec = args[0];
-            if (buf_msec < 20) {
-                buf_msec = 20;
-            }
-            unsigned long buf_size =  buf_msec * 48;
-            free(buf);
-            buf = (double *)calloc(buf_size, sizeof(double));
-            play_point = buf_length - 128;
-            rec_point = 0;
-            cout << "buf_msec is " << buf_msec << endl;
-            return {};
-        }
-     };
+//    message<> number { this, "float",
+//        MIN_FUNCTION {
+//            int buf_msec = args[0];
+//            if (buf_msec < 20) {
+//                buf_msec = 20;
+//            }
+//            unsigned long buf_size =  buf_msec * 48;
+//            free(buf);
+//            buf = (double *)calloc(buf_size, sizeof(double));
+//            play_point = buf_length - 128;
+//            rec_point = 0;
+//            cout << "buf_msec is " << buf_msec << endl;
+//            return {};
+//        }
+//     };
 
     
     // signal process
     void operator()(audio_bundle input, audio_bundle output){
+    		auto in   = input.samples(0);
         auto out = output.samples(0);
-			
-        for(auto i = 0; i < input.frame_count(); ++i){
-            if (labs(rec_point - play_point) > 128){
-                out[i] = buf[play_point];
-                play_point++;
-            }else{
-                out[i] = buf[play_point]; // output signal
-            }
-					
-            if(play_point > buf_length){
-                play_point = 0;
-            }
-        }
     };
     
     
@@ -142,8 +198,10 @@ private:
 	//Opus decoder
 	OpusDecoder *dec;
 	int err = 0;
-				
-	//lib::sync m_oscillator;
+
+	double m_playback_position		{ 0.0 };	// normalized range
+	size_t m_record_position		{ 0 };		// native range
+	double m_one_over_samplerate	{ 1.0 };
 };
 
 MIN_EXTERNAL(ext_listToSig);
--- sources/ext.listToSig_test.cpp
+++ sources/ext.listToSig_test.cpp
@@ -19,17 +19,17 @@ SCENARIO("object produces correct output") {
         ext_listToSig&              my_object = an_instance;
 
         // check that default attr values are correct
-        REQUIRE((my_object.greeting == symbol("hello world")));
+//        REQUIRE((my_object.greeting == symbol("hello world")));
 
         // now proceed to testing various sequences of events
-        WHEN("a 'bang' is received") {
-            my_object.bang();
-            THEN("our greeting is produced at the outlet") {
-                auto& output = *c74::max::object_getoutput(my_object, 0);
-                REQUIRE((output.size() == 1));
-                REQUIRE((output[0].size() == 1));
-                REQUIRE((output[0][0] == symbol("hello world")));
-            }
-        }
+//        WHEN("a 'bang' is received") {
+//            my_object.bang();
+//            THEN("our greeting is produced at the outlet") {
+//                auto& output = *c74::max::object_getoutput(my_object, 0);
+//                REQUIRE((output.size() == 1));
+//                REQUIRE((output[0].size() == 1));
+//                REQUIRE((output[0][0] == symbol("hello world")));
+//            }
+//        }
     }
 }
